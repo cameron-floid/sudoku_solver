58d51fb6786428d0a9bd375d9100a95d77159bc5
unknown <cameron@floids.co>
2023-12-15 20:41:25 +0530

    Finished the solving algorithm

--- Solver/board.py
+++ Solver/board.py
@@ -13,6 +13,10 @@ class Board:
             [0, 0, 0, 0, 8, 0, 0, 7, 9]
         ]
 
+        if not self.valid_board():
+            print("\nThe Entered Sodoku Board Is Not Valid!\n")
+            exit()
+
     def print(self, title: str = "Board"):
 
         print(f"\n{title}:\n")
@@ -32,3 +36,75 @@ class Board:
                 print(self.tiles[i][j], end=" ")
 
         print("\n")
+
+    def next_empty_tile(self):
+
+        """
+        Returns the next empty tile in the board
+        :return:
+        """
+
+        for i in range(len(self.tiles)):
+            for j in range(len(self.tiles[i])):
+                if self.tiles[i][j] == 0:
+                    return i, j
+
+        return None
+
+    def valid_board(self):
+        size = len(self.tiles)
+        for col in self.tiles:
+            if len(col) != size:
+                return False
+
+        return True
+
+    def valid(self, board, value: int, position: tuple) -> bool:
+
+        # check row
+        for i in range(len(self.tiles[0])):
+            if self.tiles[position[0]][i] == value and i != position[1]:
+                return False
+
+        # check the col
+        for i in range(len(self.tiles[0])):
+            if self.tiles[i][position[1]] == value and i != position[0]:
+                return False
+
+        # check square
+        box_start_row = position[0] // 3 * 3
+        box_start_col = position[1] // 3 * 3
+
+        i = box_start_row
+        j = box_start_col
+
+        while i < box_start_row + 3:
+            while j < box_start_col + 3:
+                if self.tiles[i][j] == value and (i, j) != position:
+                    return False
+                j += 1
+            i += 1
+
+        return True
+
+    def solve(self, board):
+
+        next_tile = self.next_empty_tile()
+
+        if not next_tile:
+            return True
+
+        else:
+
+            row, col = next_tile
+
+            for i in range(1, 10):
+                if self.valid(self.tiles, i, next_tile):
+                    self.tiles[row][col] = i
+
+                    if self.solve(self.tiles):
+                        return True
+
+                    self.tiles[row][col] = 0
+
+        return False
--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from Solver import Board
 
 board = Board()
 board.print()
+
+solved = board.solve(board.tiles)
+
+if solved:
+    board.print(title="Solved Board")
+else:
+    board.print(title="Found No Solution")
+
+
